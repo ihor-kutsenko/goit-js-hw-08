24eb7ebda0845b44ac6c579dbf01234e3d94edd7
Rammbest <igor-kutsenko@ukr.net>
2023-05-21 18:23:46 +0300

    add task1 - gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,4 +3,27 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
-console.log(1);
+
+import SimpleLightbox from 'simpleLightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryContainer = document.querySelector(".gallery");
+const result = createGallery(galleryItems);
+
+function createGallery(array) {
+  return array
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join("");
+}
+
+galleryContainer.insertAdjacentHTML("beforeend", result);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  captionPosition: "bottom",
+});
