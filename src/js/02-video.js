11e1ee60f65d3e18447fbedc48504a74e9265398
Rammbest <igor-kutsenko@ukr.net>
2023-05-27 17:08:47 +0300

    add task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const LOCALSTORAGE_KEY = "videoplayer-current-time";
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(playedVideo, 1000));
+
+const timePlay = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+if (timePlay && timePlay.seconds) {
+  player.setCurrentTime(timePlay.seconds);
+}
+
+function playedVideo(event) {
+  const currentTime = {
+    seconds: event.seconds
+  };
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(currentTime));
+}
